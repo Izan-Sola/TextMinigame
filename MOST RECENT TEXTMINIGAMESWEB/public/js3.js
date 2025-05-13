937b7ad36b0d746aca4dd78513e300b55326afbb
Izan Sola Montesinos <83542827+Izan-Sola@users.noreply.github.com>
2025-05-13 12:37:40 +0200

    Update js3.js

--- MOST RECENT TEXTMINIGAMESWEB/public/js3.js
+++ MOST RECENT TEXTMINIGAMESWEB/public/js3.js
@@ -1,78 +1,73 @@
-
 function fillGrid3() {
     // posible rank names: {Short Legged}, {Average}, {Sprinter}, {Speed Runner}
-    container = $( '#container3' )
-    container.html( '' )
-    grid3 = container.text().split( '' )
-    start = Random.integer( 0, 8 )
+    container = $('#container3')
+    container.html('')
+    grid3 = container.text().split('')
+    start = Random.integer(0, 8)
     playerStart = start
     playerEnd = 0
     currentPosition = playerStart
     coinPosition = null
 
-    for ( j = 0; j < 6; j++ ) {
+    for (j = 0; j < 6; j++) {
+        end = Random.integer(0, 8)
 
-        end = Random.integer( 0, 8 )
-
-        for ( y = 0; y < 9; y++ ) {
-
-            if ( y != start ) ( coinPosition != null && coinPosition == y ) ? (grid3.push( 'Φ' ), coinPosition = null) : grid3.push( 'X' )
-            else if ( Random.integer( barrierChance, 8 ) == barrierChance ) grid3.push( '═' )
-            else grid3.push( 'O' )
+        for (y = 0; y < 9; y++) {
+            if (y != start) {
+                (coinPosition != null && coinPosition == y) ? (grid3.push('Φ'), coinPosition = null) : grid3.push('X')
+            } else if (Random.integer(barrierChance, 8) == barrierChance) grid3.push('═')
+            else grid3.push('O')
         }
 
-        for ( i = 0; i < 9; i++ ) {
+        for (i = 0; i < 9; i++) {
             // draws path between the points
-            if ( ( i >= start && i <= end ) || ( i <= start && i >= end ) ) {
-                grid3.push( 'O' )
+            if ((i >= start && i <= end) || (i <= start && i >= end)) {
+                grid3.push('O')
                 // removes coin if it is between 2 paths to avoid making a bridge
                 l = grid3.length
-                if ( grid3[ l - 10 ] == 'Φ' && grid3[ l - 19 ] == 'O' ) grid3[ l - 10 ] = 'X'
-                if ( j == 5 ) playerEnd = grid3.length - 1
-     
-            } else grid3.push( 'X' )          
+                if (grid3[l - 10] == 'Φ' && grid3[l - 19] == 'O') grid3[l - 10] = 'X'
+                if (j == 5) playerEnd = grid3.length - 1
+            } else grid3.push('X')
             // determines the position of the coin
-            if ( i > start && i < end ) Random.integer( coinChance, 4 ) == coinChance ? coinPosition = i : coinPosition = null   
+            if (i > start && i < end) Random.integer(coinChance, 4) == coinChance ? coinPosition = i : coinPosition = null
         }
+
         start = end
     }
 
-    grid3[ playerStart ] = '+'
-    container.html( grid3 )
-    console.log( playerEnd, grid3[ playerEnd ] )
+    grid3[playerStart] = '+'
+    container.html(grid3)
+    console.log(playerEnd, grid3[playerEnd])
 }
 
-function start () {
+function start() {
     fillgrid3()
-    timerInterval = setInterval( timer, 1000 )
-    incomingDeathInterval = setInterval( incomingDeath, incomingDeathDelay )
+    timerInterval = setInterval(timer, 1000)
+    incomingDeathInterval = setInterval(incomingDeath, incomingDeathDelay)
 }
 
 function end() {
-    clearInterval( timerInterval )
-    clearInterval( incomingDeathInterval )
+    clearInterval(timerInterval)
+    clearInterval(incomingDeathInterval)
     alert("Time 0")
-
-    //sendDatabaseUpdate(totalPoints, coins, "newMaxScore3", "")
+    // sendDatabaseUpdate(totalPoints, coins, "newMaxScore3", "")
 }
 
 function incomingDeath() {
-    
-    for( i = 9 * row; i < 9 * row + 9; i++ ) grid3[ i ] = 'V'
+    for (i = 9 * row; i < 9 * row + 9; i++) grid3[i] = 'V'
     console.log(incomingDeathDelay)
-    container.html( grid3 )
+    container.html(grid3)
     row += 1
 }
 
-function timer () {
+function timer() {
     currentTime -= 1
 
-    if ( currentTime % 8 == 0 ) barrierChance += 1
-    else if ( currentTime % 16 == 0 ) coinChance += 1
-    else if ( currentTime % 4 == 0  && incomingDeathDelay > 1250 ) incomingDeathDelay -= 50
-   
-   
-    $( '.timer-points' ).html(
+    if (currentTime % 8 == 0) barrierChance += 1
+    else if (currentTime % 16 == 0) coinChance += 1
+    else if (currentTime % 4 == 0 && incomingDeathDelay > 1250) incomingDeathDelay -= 50
+
+    $('.timer-points').html(
         'Time left: ' + currentTime + ' (s)' +
         ' Points: ' + points +
         ' Coins: ' + coins +
@@ -81,78 +76,55 @@ function timer () {
         ' Incoming Death Delay: ' + incomingDeathDelay
     )
 
-    if ( currentTime == 0 ) end()
+    if (currentTime == 0) end()
 }
 
-$( document ).on( 'keydown', function ( k ) {
-
+$(document).on('keydown', function(k) {
     position = currentPosition
     pastPosition = position
 
-    if ( coinPos != null ) {
-        grid3[ coinPos ] = 'O'
+    if (coinPos != null) grid3[coinPos] = 'O'
+
+    switch (k.key) {
+        case 'ArrowUp': position -= 9; break       
+        case 'ArrowLeft': position -= 1; break
+        case 'ArrowDown': position += 9; break
+        case 'ArrowRight': position += 1; break
+        case ' ': if (grid3[position + 9] == '═') position += 18
     }
 
-    switch ( k.key ) {
-        case 'ArrowUp':
-            position -= 9
-            break
-        case 'ArrowLeft':
-            position -= 1
-            break
-        case 'ArrowDown':
-            position += 9
-            break
-        case 'ArrowRight':
-            position += 1
-            break
-        case ' ':
-            if ( grid3[ position + 9 ] == '═' ) position += 18
-            
-            break
-    }
+    if (k.key != ' ' && grid3[position] == '═') position = pastPosition
 
-    if ( k.key != ' ' && grid3[ position ] == '═' ) position = pastPosition
-
-    if ( position == playerEnd ) {
+    if (position == playerEnd) {
         fillgrid3()
         points += t + currentTime
         clearInterval(incomingDeathInterval)
-
-        setTimeout( () => {
-            setInterval( incomingDeath, incomingDeathDelay )
-        }, 2000 )
-
+        setTimeout(() => { setInterval(incomingDeath, incomingDeathDelay) }, 2000)
         row = 0
-    } 
-    else if ( !( position > grid3.length ) && !( position < grid3.length - grid3.length - 1 ) ) {
-
-        switch ( grid3[ position ] ) {
-
+    } else if (!(position > grid3.length) && !(position < 0)) {
+        switch (grid3[position]) {
             case 'X':
-                container.html( 'You stepped out of the path!' )
-                end();
+                container.html('You stepped out of the path!')
+                end()
                 break
-
             case 'O':
-                grid3[ currentPosition ] = 'O'
-                grid3[ position ] = '+'
-                container.html( grid3 )
+                grid3[currentPosition] = 'O'
+                grid3[position] = '+'
+                container.html(grid3)
                 break
-
             case 'Φ':
-                coinPos = grid3.indexOf( 'Φ' )
-                grid3[ currentPosition ] = 'O'
-                grid3[ position ] = '+'
-                container.html( grid3 )
-                points += 20 + ( 20 * ( currentTime / 100 ) )
+                coinPos = grid3.indexOf('Φ')
+                grid3[currentPosition] = 'O'
+                grid3[position] = '+'
+                container.html(grid3)
+                points += 20 + (20 * (currentTime / 100))
                 break
         }
         currentPosition = position
     }
-} )
+})
 
-$( document ).ready( function () {
+$(document).ready(function() {
     Random = new Random.Random()
     currentTime = 60
     t = currentTime
@@ -164,7 +136,7 @@ $( document ).ready( function () {
     incomingDeathDelay = 1500
     row = 0
 
-    $( '.timer-points' ).html(
+    $('.timer-points').html(
         'Time left: ' + currentTime + ' (s)' +
         ' Points: ' + points +
         ' Coins: ' + coins +
@@ -172,4 +144,4 @@ $( document ).ready( function () {
         ' Paths completed: ' + 0
     )
     // fillGrid3()
-} )
+})
