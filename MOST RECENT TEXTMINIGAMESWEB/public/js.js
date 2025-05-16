a5d3a4233fd664d3daa607f95f59a6125a0898fb
Izan Sola Montesinos <83542827+Izan-Sola@users.noreply.github.com>
2025-05-16 09:16:03 +0200

    refactorized stupid code

--- MOST RECENT TEXTMINIGAMESWEB/public/js.js
+++ MOST RECENT TEXTMINIGAMESWEB/public/js.js
@@ -27,8 +27,6 @@ function fillGrid() {
   $('.container').html(grid);
 }
 
-
-
 $(document).ready(function() {
   
   sendDatabaseUpdate(score, 0, "updateScoreList1");
@@ -36,50 +34,18 @@ $(document).ready(function() {
     $('.namePrompt').css('visibility', 'visible'); 
   fillGrid();
 
-   /*   Useless feature, replaced
-    scores = [];
-    for(i=1; i<=4; i++) {
-      if(localStorage.getItem('score'+i) !== null) {
-          scores.push(localStorage.getItem('score'+i));
-      }
-    }
-    maxScore = Math.max(...scores);
-    replaced = false;
-    for(i=1; i<=4; i++) { 
-         
-        if(localStorage.getItem('score'+i) == localStorage.getItem('maxScore')) {
-            localStorage.removeItem('score'+i)
-      }
-        if(score > localStorage.getItem('score'+i) && replaced == false) {
-          localStorage.setItem('score'+i, score)
-          replaced = true;
-        }
-        if(localStorage.getItem('score'+i) !== null) {
-           $('.container').append('<p class="scores">Score: '+localStorage.getItem('score'+i)+'</p>')  
-      }
-    }
-      */
-
   $(document).on('keydown', function(k) {
-k.stopPropagation();
-if( $('.container').css('visibility') == 'visible') {
-    if (k.key == 'ArrowLeft' && position != 0) {
+  k.stopPropagation();
 
-      move("backward");
-    }
-    if (k.key == 'ArrowRight' && position != 53) {
+    if( $('.container').css('visibility') == 'visible') {
 
-      move("forward");
-    }
-    if (k.key == 'ArrowUp' && position != 0) {
+        if (k.key == 'ArrowLeft' && position != 0) move(position-=1)
+        else if (k.key == 'ArrowRight' && position != 53) move(position+=1)
+        else if (k.key == 'ArrowUp' && position != 0) move(position-=9) 
+        else if (k.key == 'ArrowDown' && position != 53) move(position+=9)
+        
+      }
 
-      move("up");
-    }
-    if (k.key == 'ArrowDown' && position != 53) {
-
-      move("down");
-    }
-  }
   })
 })
 
@@ -112,39 +78,9 @@ function bombSpawn() {
   }
 }
 
-/* nope
- grid.shift();
- grid.push("*");
- dot = grid[grid.length-1];
- grid.splice(grid.length-1, 1)
- grid.splice(dotPos, 0, dot)*/
 
-function move(direction) {
-  switch (direction) {
+function move(position) {
 
-    case 'forward':
-      position = position + 1
-      break;
-
-    case 'backward':
-      position = position - 1
-      break;
-
-    case 'up':
-      position = position - 9
-      if(position < 0) {
-        position+=9;
-      }
-      break;
-
-    case 'down':
-      position = position + 9
-      if(position > grid.length-1) {
-        position-=9;
-      }
-      break;
-
-  }
   console.log(position)
   if (!(position > grid.length) && !(position < grid.length - grid.length - 1)) {
 
