832ee71cd624d25ab72200817f0ffd09b408ca2b
Izan Sola Montesinos <83542827+Izan-Sola@users.noreply.github.com>
2025-05-16 09:15:02 +0200

    Update js.js

--- LocallyPlayableMinigames/(old version no server or database) - textminigame/js.js
+++ LocallyPlayableMinigames/(old version no server or database) - textminigame/js.js
@@ -1,174 +1,95 @@
+
+
+let restart, coinCount, position, currentPosition, score, moves, coins, grid;
+let min, max, dotPos, bombPos, maxLength;
+let element;
+let coinInterval, bombInterval;
+
 function fillGrid() {
+  coinInterval = setInterval(coinSpawn, 2000);
+  bombInterval = setInterval(bombSpawn, 4000);
+  restart = false
+  coinCount = 0
   position = 0
   currentPosition = 0
   score = 0
   moves = 5
-
-grid = ['+', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
+  coins = 0
+  grid = ['+', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
         'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
         'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
         'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
         'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O',
         'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O', 'O']; 
 
-        $('.container').html(grid);
+  $('.container').html(grid);
 }
 
-function test() {
-  if(grid.length > maxLength) {
-    grid.pop();
-  }
-}
+
+
 $(document).ready(function() {
+  
+  //sendDatabaseUpdate(score, 0, "updateScoreList1");
 
+    $('.namePrompt').css('visibility', 'visible'); 
   fillGrid();
 
-  $('input').on('mousedown', function(event){
-    event.stopPropagation();
-
-  if(this.id == 'start') {
-      score=0
-      $('.scoreCounter').html('<b>Current Score: 0</b>');
-      fillGrid();
-      coinInterval = setInterval(coinSpawn, 2000);
-      bombInterval = setInterval(bombSpawn, 4000);
-      testInterval = setInterval(test, 1000);
-  }
-
-  if(this.id == 'pause') {
-    clearInterval(bombInterval)
-    clearInterval(coinInterval)
-  }
-
-  if(this.id == 'scoreListButton') {
-
-    scores = [];
-    for(i=1; i<=4; i++) {
-      if(localStorage.getItem('score'+i) !== null) {
-          scores.push(localStorage.getItem('score'+i));
-      }
-    }
-    
-    maxScore = Math.max(...scores);
-    if(maxScore > localStorage.getItem('maxScore')) {
-         localStorage.setItem('maxScore', maxScore)
-    }
-
-    $('.container').html('<h3 class="highestScore">Highest: '+localStorage.getItem('maxScore')+'</h3>')
-    
-    replaced = false;
-    for(i=1; i<=4; i++) { 
-         
-        if(localStorage.getItem('score'+i) == localStorage.getItem('maxScore')) {
-            localStorage.removeItem('score'+i)
-      }
-        if(score > localStorage.getItem('score'+i) && replaced == false) {
-          localStorage.setItem('score'+i, score)
-          replaced = true;
-        }
-        if(localStorage.getItem('score'+i) !== null) {
-           $('.container').append('<p class="scores">Score: '+localStorage.getItem('score'+i)+'</p>')  
-      }
-    }
-    score=0;
-  }
-  })
-
   $(document).on('keydown', function(k) {
+  k.stopPropagation();
 
-    if (k.key == 'ArrowLeft' && position != 0) {
+    if( $('.container').css('visibility') == 'visible') {
 
-      move("backward");
-    }
-    if (k.key == 'ArrowRight' && position != 53) {
+        if (k.key == 'ArrowLeft' && position != 0) move(position-=1)
+        else if (k.key == 'ArrowRight' && position != 53) move(position+=1)
+        else if (k.key == 'ArrowUp' && position != 0) move(position-=9) 
+        else if (k.key == 'ArrowDown' && position != 53) move(position+=9)
+        
+      }
 
-      move("forward");
-    }
-    if (k.key == 'ArrowUp' && position != 0) {
-
-      move("up");
-    }
-    if (k.key == 'ArrowDown' && position != 53) {
-
-      move("down");
-    }
   })
 })
 
-
-function saveScore() {
-    for(i=1; i<4; i++) {
-      if(localStorage.getItem('score'+i) === null) {
-         localStorage.setItem('score'+i, score);
-         break;
-      }
-    }
-}
-
 function coinSpawn() {
 
+  if(coinCount <=1) {
+
   min = Math.ceil(2);
-  max = Math.floor(grid.length - 1);
+  max = Math.floor(grid.length - 2);
   dotPos = Math.floor(Math.random() * (max - min + 1)) + min;
 
   grid.forEach(function(element, index, array) {
-    if (dotPos != currentPosition) {
+    if (dotPos != currentPosition && grid[dotPos] != '@') {
 
       grid[dotPos] = '*';
       $('.container').html(grid);
     }
   })
+  coinCount+=1
 }
-
+}
 function bombSpawn() {
   min = Math.ceil(0);
-  max = Math.floor(grid.length - 1);
+  max = Math.floor(grid.length - 2);
   bombPos = Math.floor(Math.random() * (max - min + 1)) + min;
 
-  if (bombPos != currentPosition) {
+  if (bombPos != currentPosition && grid[bombPos] != '*') {
     grid[bombPos] = '@';
    // mvBombInterval = setInterval(moveBomb, 2000)
   }
 }
 
 
-/* nope
- grid.shift();
- grid.push("*");
- dot = grid[grid.length-1];
- grid.splice(grid.length-1, 1)
- grid.splice(dotPos, 0, dot)*/
+function move(position) {
 
-function move(direction) {
-
-  switch (direction) {
-
-    case 'forward':
-      position = position + 1
-      break;
-
-    case 'backward':
-      position = position - 1
-      break;
-
-    case 'up':
-      position = position - 9
-      break;
-
-    case 'down':
-      position = position + 9
-      break;
-
-  }
+  console.log(position)
   if (!(position > grid.length) && !(position < grid.length - grid.length - 1)) {
 
     if (grid[position] == '@') {
-       // clearInterval(mvBombInterval)
         clearInterval(bombInterval)
         clearInterval(coinInterval)
         grid.length = 0;
         grid.push('You touched a bomb! You lost.')
-        saveScore();
+        //sendDatabaseUpdate(score, coins, "newMaxScore1", "")
     }
 
     if (grid[position] == '*') {
@@ -177,6 +98,7 @@ function move(direction) {
         maxLength = grid.length
         score += 1;
         $('.scoreCounter').html('<b>Current Score: ' + score + '</b>');
+        coinCount-=2;
     }
     element = grid[currentPosition];
     grid.splice(currentPosition, 1);
@@ -186,3 +108,5 @@ function move(direction) {
     currentPosition = position;
   }
 }
+
+
